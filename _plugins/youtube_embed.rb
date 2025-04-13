808bb12989fbd2362e0bb5ba8fec7253fc6a1cf2
Sergii Illiukhin <s.illyuhin@gmail.com>
2025-04-13 15:39:28 +0300

    Update youtube_embed.rb

--- _plugins/youtube_embed.rb
+++ _plugins/youtube_embed.rb
@@ -3,15 +3,14 @@ module Jekyll
     def youtube_embed(input)
       return input unless input.is_a?(String)
       
-      # Match YouTube URLs in various formats
-      youtube_regex = %r{
-        (?:https?://)?
-        (?:www\.)?
-        (?:youtube\.com/watch\?v=|youtu\.be/)
-        ([a-zA-Z0-9_-]+)
-      }x
+      # First handle [embed] tags
+      input = input.gsub(/\[embed\](https?:\/\/)?(www\.)?(youtube\.com\/watch\?v=|youtu\.be\/)([a-zA-Z0-9_-]+)(\?[^\]]*)?\[\/embed\]/i) do |match|
+        video_id = $4
+        %Q{<div class="youtube-embed"><iframe width="560" height="315" src="https://www.youtube.com/embed/#{video_id}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>}
+      end
       
-      input.gsub(youtube_regex) do |match|
+      # Then handle direct URLs
+      input.gsub(/(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/watch\?v=|youtu\.be\/)([a-zA-Z0-9_-]+)(?:\s|$)/) do |match|
         video_id = $1
         %Q{<div class="youtube-embed"><iframe width="560" height="315" src="https://www.youtube.com/embed/#{video_id}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>}
       end
