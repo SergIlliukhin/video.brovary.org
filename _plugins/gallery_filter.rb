740b9502eaf4873056d537ab64a4f59a01f65c8d
Sergii Illiukhin <s.illyuhin@gmail.com>
2025-04-18 16:33:43 +0300

    debug

--- _plugins/gallery_filter.rb
+++ _plugins/gallery_filter.rb
@@ -1,19 +1,30 @@
 module Jekyll
+  class GalleryGenerator < Generator
+    def generate(site)
+      # Load CSV file
+      csv_path = File.join(site.source, 'assets', 'wp_images.csv')
+      if File.exist?(csv_path)
+        require 'csv'
+        site.data['wp_images'] = []
+        CSV.foreach(csv_path, headers: true) do |row|
+          site.data['wp_images'] << {
+            'id' => row['id'],
+            'url' => row['url'],
+            'title' => row['title'] || ''
+          }
+        end
+      end
+    end
+  end
+
   module GalleryFilter
     def transform_gallery(input)
       return input unless input.is_a?(String)
       
       # Handle gallery shortcodes with escaped brackets
-      input = input.gsub(/\\\[gallery[^\]]*ids=["']([^"'\]]+)["'][^\]]*\\\]/) do |match|
+      input.gsub(/\\\[gallery[^\]]*ids=["']([^"'\]]+)["'][^\]]*\\\]/) do |match|
         process_gallery($1)
       end
-      
-      # Handle gallery shortcodes with unescaped brackets
-      input = input.gsub(/\[gallery[^\]]*ids=["']([^"'\]]+)["'][^\]]*\]/) do |match|
-        process_gallery($1)
-      end
-      
-      input
     end
     
     private
@@ -25,41 +36,25 @@ module Jekyll
       gallery_html = '<div class="gallery">'
       
       ids.each do |id|
-        # Try to find image in site data
-        image = begin
-          site = @context.registers[:site]
-          if site && site.data && site.data['wp_images']
-            site.data['wp_images'].find { |img| img['id'].to_s == id.to_s }
-          else
-            # Try to load from CSV file directly
-            csv_path = File.join(site.source, 'assets', 'wp_images.csv')
-            if File.exist?(csv_path)
-              require 'csv'
-              CSV.foreach(csv_path, headers: true) do |row|
-                if row['id'].to_s == id.to_s
-                  return {
-                    'id' => row['id'],
-                    'url' => row['url'],
-                    'title' => row['title'] || ''
-                  }
-                end
-              end
-            end
-            nil
-          end
-        rescue
-          nil
-        end
-        
+        image = find_image(id)
         if image
           filename = File.basename(image['url'].to_s)
-          new_url = "#{@context.registers[:site].config['baseurl']}/assets/images/#{filename}"
+          new_url = "/assets/images/#{filename}"
           gallery_html += %Q{<img src="#{new_url}" alt="#{image['title']}" class="gallery-image">}
         end
       end
       
       gallery_html += '</div>'
       gallery_html
+    end
+    
+    def find_image(id)
+      return nil unless @context && @context.registers[:site]
+      
+      site = @context.registers[:site]
+      return nil unless site.data && site.data['wp_images']
+      
+      site.data['wp_images'].find { |img| img['id'].to_s == id.to_s }
     end
   end
 end
